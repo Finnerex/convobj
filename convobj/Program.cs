a2962b6cba6682f13a97cccad3caaaf98b9659df
Finnerex <61033009+Finnerex@users.noreply.github.com>
2023-12-14 19:50:20 -0500

    works for entire folder

--- convobj/Program.cs
+++ convobj/Program.cs
@@ -14,50 +14,83 @@ internal class Program
     public static void Main(string[] args)
     {
 
-        Obj theObject = new Obj();
-            
-        Console.Write("Enter file name: ");
-        string name = Console.ReadLine().Replace(".obj", "");
-            
-        Console.Write("Enter file path (not including file): "); // this kind of stuff wont have to exist eventually
+        Console.Write("Enter project objs folder path (hit enter for last path): ");
         string path = Console.ReadLine();
-        if (path[path.Length - 1] != '\\')
-            path += '\\';
 
-        theObject.LoadObj(path + name + ".obj");
+        if (path == "")
+            path = new StreamReader("cachedPath.txt").ReadLine();
         
-        Console.WriteLine("Exporting data...");
+        else
+        {
+            if (path[path.Length - 1] != '\\')
+                path += '\\';
 
-        StringBuilder faces = ExportFaces(theObject.FaceList);
-        StringBuilder vertices = ExportVertices(theObject.VertexList);
+            StreamWriter cache = new StreamWriter("cachedPath.txt");
+            cache.WriteLine(path);
+            cache.Close();
+        }
+        
 
-        int numFaces = theObject.FaceList.Count;
-        int numVerts = theObject.VertexList.Count;
+        StreamReader converts = new StreamReader(path + "converts.txt");
         
+        StreamWriter objsH = new StreamWriter(path + "objs.h");
+        objsH.WriteLine("#ifndef OBJS_H\n#define OBJS_H\n");
+
         
-        Console.WriteLine("Writing C file...");
+        string name = converts.ReadLine();
+
+        while (name != null)
+        {
+            name = name.Replace(".obj", "");
+            
+            objsH.WriteLine($"#include \"{name}.h\"");
+            
+            Obj theObject = new Obj();
+    
+            theObject.LoadObj(path + name + ".obj");
+            
+            Console.WriteLine("Exporting data ...");
+    
+            string faces = ExportFaces(theObject.FaceList).ToString();
+            string vertices = ExportVertices(theObject.VertexList).ToString();
+            
+            WriteCFile(name, path, theObject.FaceList.Count, theObject.VertexList.Count, faces, vertices);
+            WriteHFile(name, path);
+
+            name = converts.ReadLine();
+        }
+
+        objsH.WriteLine("\n#endif");
+        objsH.Close();
         
-        StreamWriter cFile = new StreamWriter(name + ".c");
+        Console.WriteLine("Done!");
+
+    }
+
+    private static void WriteCFile(string name, string path, int numFaces, int numVerts, string faces, string vertices)
+    {
+        Console.WriteLine($"Writing {name}.c ...");
+
+        StreamWriter cFile = new StreamWriter(path + name + ".c");
         cFile.WriteLine($"#include \"{name}.h\" \n");
         cFile.WriteLine($"face_t {name}_faces[{numFaces}] = {faces}; \n\nvec3_t {name}_verts[{numVerts}] = {vertices};\n");
         cFile.WriteLine($"mesh_t {name}_data = {{ {name}_faces, {name}_verts, {numFaces}, {numVerts} }};");
         cFile.Close();
+    }
+    
+    private static void WriteHFile(string name, string path)
+    {
+        Console.WriteLine($"Writing {name}.h ...");
 
-        
-        Console.WriteLine("Writing header file...");
-
-        StreamWriter hFile = new StreamWriter(name + ".h");
+        StreamWriter hFile = new StreamWriter(path + name + ".h");
         hFile.WriteLine($"#ifndef {name.ToUpper()}_H\n#define {name.ToUpper()}_H");
-        hFile.WriteLine("#include \"object.h\" \n#include \"vector.h\" \n");
+        hFile.WriteLine("#include \"../object.h\" \n#include \"../vector.h\" \n"); // maybe theres a way of not doing ../
         
         hFile.WriteLine($"#define {name}_mesh &{name}_data");
         hFile.WriteLine($"extern mesh_t {name}_data;\n");
         
         hFile.WriteLine("#endif");
         hFile.Close();
-        
-        Console.WriteLine("Done!");
-
     }
 
     private static StringBuilder ExportVertices(List<Vertex> vertexList)
@@ -68,12 +101,12 @@ internal class Program
         {
             Vertex vert = vertexList[i];
 
-            vertices.Append($"{{ {(float)vert.X:F1}f, {(float)vert.Y:F1}f, {(float)vert.Z:F1}f }}");
+            vertices.Append($"{{ {(float)vert.X}, {(float)vert.Y}, {(float)vert.Z} }}"); // f you
 
             if (i < vertexList.Count - 1)
                 vertices.Append(", ");
                 
-            if (i % 6 == 5 && i < vertexList.Count - 1)
+            if (i % 4 == 3 && i < vertexList.Count - 1)
                 vertices.Append("\n");
 
         }
@@ -101,7 +134,7 @@ internal class Program
             if (i < faceList.Count - 1)
                 faces.Append(", ");
                 
-            if (i % 8 == 7 && i < faceList.Count - 1)
+            if (i % 6 == 5 && i < faceList.Count - 1)
                 faces.Append("\n");
 
         }
